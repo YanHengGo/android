58087187ead339d00795ce4e548e28369f8a313a
YanHengGo <dang712@hotmail.com>
2018-10-14 00:52:51 +0900

    ボールアニメーション効果追加

--- animation3/app/src/main/java/com/animation3/yanheng/MainActivity.java
+++ animation3/app/src/main/java/com/animation3/yanheng/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends Activity {
                 img_ball.startAnimation(animationSet);
             }
         });
-        btn_stop = (Button)findViewById(R.id.btn_stop);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +48,7 @@ public class MainActivity extends Activity {
         animationSet.addAnimation(scaleAnimation);
         animationSet.addAnimation(translateAnimation);
     }
+
     //時間
     private final int DURATION_TIME = 2000;
     //回数
@@ -58,12 +59,13 @@ public class MainActivity extends Activity {
     private final int REPEAT_MODE = Animation.RESTART; //REVERSE RESTART
     //大きさアニメーション
     private ScaleAnimation scaleAnimation;
+
     {
         scaleAnimation = new ScaleAnimation(
-                1f,0.5f,                                 //Xの開始大きさ
-                1f,0.5f,                                 //Yの開始大きさ
-                Animation.RELATIVE_TO_SELF,0.5f,       //開始位置の位置X
-                Animation.RELATIVE_TO_SELF,0.5f        //開始位置の位置Y
+                1f, 0.5f,                                 //Xの開始大きさ
+                1f, 0.5f,                                 //Yの開始大きさ
+                Animation.RELATIVE_TO_SELF, 0.5f,       //開始位置の位置X
+                Animation.RELATIVE_TO_SELF, 0.5f        //開始位置の位置Y
         );
         scaleAnimation.setDuration(DURATION_TIME);
         scaleAnimation.setRepeatCount(REPEAT_COUNT);
@@ -72,14 +74,15 @@ public class MainActivity extends Activity {
 
     //移動アニメーション初期化
     private TranslateAnimation translateAnimation;
+
     {
         //Animation.RELATIVE_TO_PARENT　　親のViewに対して
         //Animation.RELATIVE_TO_SELF      自分のViewに対して
         translateAnimation = new TranslateAnimation(
-                Animation.RELATIVE_TO_PARENT,0, //"X"開始位置のタイプとValue
-                Animation.RELATIVE_TO_PARENT,0f, //"X"終了位置のタイプとValue
-                Animation.RELATIVE_TO_PARENT,0, // "Y"開始位置のタイプとValue　
-                Animation.RELATIVE_TO_PARENT,0.9f //　"Y"終了位置のタイプとValue
+                Animation.RELATIVE_TO_PARENT, 0, //"X"開始位置のタイプとValue
+                Animation.RELATIVE_TO_PARENT, 0f, //"X"終了位置のタイプとValue
+                Animation.RELATIVE_TO_PARENT, 0, // "Y"開始位置のタイプとValue　
+                Animation.RELATIVE_TO_PARENT, 0.9f //　"Y"終了位置のタイプとValue
         );
         //アニメーションの時間
         translateAnimation.setDuration(DURATION_TIME);
