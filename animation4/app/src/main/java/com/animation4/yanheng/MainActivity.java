69c0977c72bb3be0802d8b29694b837f7588f1a6
YanHengGo <dang712@hotmail.com>
2018-10-14 14:02:22 +0900

    イメージ効果追加

--- animation4/app/src/main/java/com/animation4/yanheng/MainActivity.java
+++ animation4/app/src/main/java/com/animation4/yanheng/MainActivity.java
@@ -11,8 +11,9 @@ public class MainActivity extends Activity {
 
     private ImageView imageView;
     private Button btn_start;
-    private Button btn_stop;
+    private Button btn_heart;
     private ObjectAnimator objectAnimator;
+    private Button btn_tellphone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +23,9 @@ public class MainActivity extends Activity {
     }
 
     private void initView() {
-        imageView = (ImageView) findViewById(R.id.img_hert);
-        boolean isHertTest = false;
-        if(isHertTest){
-            imageView.setImageResource(R.mipmap.hert);
-            objectAnimator = AnimationUtil2.getHertAnimation(imageView);
-        }else{
-            imageView.setImageResource(R.mipmap.tellphone);
-            objectAnimator = AnimationUtil2.getTellphoneAnimation(imageView);
-        }
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.mipmap.heart);
+        objectAnimator = AnimationUtil2.getHertAnimation(imageView);
 
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_start.setOnClickListener(new View.OnClickListener() {
@@ -39,12 +34,25 @@ public class MainActivity extends Activity {
                 objectAnimator.start();
             }
         });
-        btn_stop = (Button) findViewById(R.id.btn_stop);
-        btn_stop.setOnClickListener(new View.OnClickListener() {
+        btn_heart = (Button) findViewById(R.id.btn_heart);
+        btn_heart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 objectAnimator.cancel();
+                imageView.setImageResource(R.mipmap.heart);
+                objectAnimator = AnimationUtil2.getHertAnimation(imageView);
             }
         });
+        btn_tellphone = (Button) findViewById(R.id.btn_tellphone);
+        btn_tellphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                objectAnimator.cancel();
+                imageView.setImageResource(R.mipmap.tellphone);
+                objectAnimator = AnimationUtil2.getTellphoneAnimation(imageView);
+            }
+        });
+
     }
+
 }
